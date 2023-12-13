400775f99633e6f05238f3a8240284050328c205
irfan.mulic <irfan.mulic@servicenow.com>
2023-12-13 14:42:20 -0600

    component code

--- app.js
+++ app.js
@@ -12,5 +12,18 @@ document.getElementById('submit').addEventListener('click', function() {
     document.querySelector('.rating').innerText = starSelected;
     document.querySelector('.thank-you').style='display: flex';
     document.querySelector('.intro').style='display: none';
+    //generateConfetti();
 
 });
+
+function generateConfetti() {
+    debugger;
+    const confettiContainer = document.querySelector(".thank-you");
+
+    for (let i = 0; i < 50; i++) {
+        const confetti = document.createElement("div");
+        confetti.className = "confetti";
+        confetti.style.left = Math.random() * 100 + "vw";
+        confettiContainer.appendChild(confetti);
+    }
+}
